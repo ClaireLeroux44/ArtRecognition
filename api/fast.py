aa6e1a43ef4114a17921eff4bed9586d17169507
mariedos <marie.daussee@gmail.com>
2020-12-14 14:40:59 +0100

    updating fast.py

--- api/fast.py
+++ api/fast.py
@@ -8,6 +8,7 @@ from io import BytesIO
 import os
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img
+import pandas as pd
 
 
 app = FastAPI()
@@ -24,25 +25,29 @@ def check_extension(filename):
     else :
         return True
 
-def path_to_image(path, image_size, num_channels, interpolation):
-    img = io_ops.read_file(path)
-    img = image_ops.decode_image(img, channels=num_channels, expand_animations=False)
-    img = image_ops.resize_images_v2(img, image_size, method=interpolation)
-    img.set_shape((image_size[0], image_size[1], num_channels))
-    return img
+# def path_to_image(path, image_size, num_channels, interpolation):
+#     img = io_ops.read_file(path)
+#     img = image_ops.decode_image(img, channels=num_channels, expand_animations=False)
+#     img = image_ops.resize_images_v2(img, image_size, method=interpolation)
+#     img.set_shape((image_size[0], image_size[1], num_channels))
+#     return img
 
 def read_imagefile(file) -> Image.Image:
-    #img = Image.open(BytesIO(file))
-    img_test = path_to_image(file, (224, 224), 3, 'bilinear')
-    img_test = np.array(img_test)
-    img_test = np.expand_dims(img_test, axis = 0)
-    return img_test
+    img = Image.open(file)
+    img = img.resize((224,224),resample=Image.BILINEAR)
+    rgbimg = Image.new("RGB", img.size)
+    rgbimg.paste(img)
+    rgbimg = np.array(rgbimg)
+    rgbimg = np.expand_dims(rgbimg, axis = 0)
+    print(rgbimg.shape)
+    return rgbimg
+
 
 @app.on_event("startup")
 async def startup_event():
     print("loading model ...")
     dirname = os.path.dirname(os.path.dirname(__file__))
-    model_path = os.path.join(dirname,'models','20201210_170338_VGG16_v2_0')
+    model_path = os.path.join(dirname,'models','model_v5')
     model = load_model(model_path)
     cache_models["model_1"] = model
 
@@ -69,11 +74,30 @@ async def predict_handler(response : Response, inputImage : UploadFile = File(..
     #img = read_imagefile( await inputImage.read())
     img = read_imagefile(inputImage.file)
 
+
     #prediction
     model = cache_models["model_1"]
     pred = model.predict(img)
     artiste_index = np.argmax(pred[0])
+    print(artiste_index)
+    dico_artistes = {0: '_1',
+    1: '_10',
+    2: '_11',
+    3: '_12',
+    4: '_2',
+    5: '_3',
+    6: '_4',
+    7: '_5',
+    8: '_6',
+    9: '_7',
+    10: '_8',
+    11: '_9'}
+    dirname = os.path.dirname(os.path.dirname(__file__))
+    db_path = os.path.join(dirname,'ArtRecognition','data','database.csv')
+    data = pd.read_csv(db_path)
+    data_artist =data[data.loc[:,'artist_number'] == dico_artistes[artiste_index]]
+    artist_name = list(data_artist['artist'])[0]
 
-    response_payload = {"prediction" : str(artiste_index)}
+    response_payload = {"prediction" : artist_name}
 
-    return response_payload
+    return response_payload["prediction"]
